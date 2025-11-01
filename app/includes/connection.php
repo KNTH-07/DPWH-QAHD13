52f50b313997c4f292f55fdfe83a05b6903fb55a
KNTH <124411703+KNTH-07@users.noreply.github.com>
2025-11-02 03:37:48 +0800

    Delete app directory

--- app/includes/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Create connection
-$connection = new mysqli("localhost", "root", "", "qahd_db");
-if ($connection->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
